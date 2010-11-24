9f9d776af5de8c9705c3a9650f96a290025c72ad
multifinger <multifinger@multifinger.(none)>
2010-11-24 16:57:49 +0300

    ADD: google analytics

--- apps/frontend/templates/vkplayer.php
+++ apps/frontend/templates/vkplayer.php
@@ -13,6 +13,16 @@
                 savePlaylist : <?php echo json_encode(url_for("vkplayer/savePlaylist")); ?>,
                 loadPlaylist : <?php echo json_encode(url_for("vkplayer/loadPlaylist")); ?>
             }
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-11626202-2']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
         <?php include_javascripts() ?>
     </head>
